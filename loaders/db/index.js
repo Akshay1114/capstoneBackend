4757a4cb340e1c3034f3e6e3124a7f72593d9251
Khushpreet Kaur <khushpreet0630@gmail.com>
2025-05-25 18:35:50 -0700

    Sign_In

--- loaders/db/index.js
+++ loaders/db/index.js
@@ -5,16 +5,16 @@ dotenv.config();
 // const uri = '';
 const uri = process.env.URI;
 console.log('uri', uri);
-const connectDB = async () => {
-  console.log('Connecting to DB');
-  mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-      .then(() => console.log('Connected Successfully'))
-      .catch((err) => {
-        console.log('Error:', err);
-        console.error('Not Connected')
-      });
-}
-  export { connectDB };
+// const connectDB = async () => {
+//   console.log('Connecting to DB');
+//   mongoose.connect(uri, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// })
+//       .then(() => console.log('Connected Successfully'))
+//       .catch((err) => {
+//         console.log('Error:', err);
+//         console.error('Not Connected')
+//       });
+// }
+  // export { connectDB };
