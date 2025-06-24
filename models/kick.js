c0eea026f4bab470cf359cdbc7447cf0a83bb1c6
preeti-davinderr <preetibhutta02@gmail.com>
2025-06-24 02:21:45 -0700

    davinder-kicks-crud-api

--- models/kick.js
+++ models/kick.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const kickSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  date: { type: Date, required: true },
+  date: { type: String, required: true },
   time: { type: String, required: true },
   count: { type: Number, required: true },
 }, { timestamps: true });
