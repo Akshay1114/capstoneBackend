d6d524e9bb5ff2f8078b3ae1c83d0178b2d15d97
preeti-davinderr <preetibhutta02@gmail.com>
2025-07-16 01:23:02 -0700

    due date added

--- models/index.js
+++ models/index.js
@@ -1,40 +1,51 @@
 import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema({
-
-  name: {
-    type: String,
-    required: true
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    phone: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["self", "other"],
+      default: "self",
+    },
+    image: {
+      type: String,
+      default: "",
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+    familyCode: {
+      type: String,
+    },
+    nickName: {
+      type: String,
+    },
+    dueDate: {
+      type: String,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  phone: {
-    type: String,
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  role: { type: String, enum: ["self", "other"], default: "self" },
-
-  image: {
-    type: String,
-    default: ''
-  },
-  isDeleted: {
-    type: Boolean,
-    default: false
-  },
-  familyCode: {
-    type: String,
-  },
-}, {
-  timestamps: true
-}
+  {
+    timestamps: true,
+  }
 );
-const User = mongoose.model('User', userSchema);
+
+const User = mongoose.model("User", userSchema);
 
 export { User };
